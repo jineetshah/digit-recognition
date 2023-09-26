64cb2f9ca6463650c0533de39f50cb98cc74d1e4
jineetshah <102129918+jineetshah@users.noreply.github.com>
2023-09-26 11:19:27 +0530

    saving image

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import gradio as gr
 import requests
 import pandas as pd
+from PIL import Image
+import numpy as np
+import base64
 
 API_URL = "https://api-inference.huggingface.co/models/AliGhiasvand86/gisha_digit_recognition"
 headers = {"Authorization": "Bearer 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"}
@@ -10,8 +13,19 @@ def query(image_path):
         response = requests.post(API_URL, headers=headers, files={"file": file})
     return response.json()
 
+def save_array_as_image(array, image_path):
+    # Convert the array to an image
+    image = Image.fromarray(array)
+    
+    # Save the image to the specified path
+    image.save(image_path)
+
 def classify_digit(image):
-    result = query(image.name)
+    # Save the image as a .png file
+    image_path = "sketchpad.png"
+    save_array_as_image(image, image_path)
+    
+    result = query(image_path)
     df = pd.DataFrame(result)
     return df
 
