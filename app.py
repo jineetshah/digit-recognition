fbb887ae233fb5b25dab2ce5216b2db409d5981e
jineetshah <102129918+jineetshah@users.noreply.github.com>
2023-09-26 10:50:03 +0530

    using tempfile

--- app.py
+++ app.py
@@ -6,32 +6,34 @@ import pandas as pd
 from PIL import Image
 import io
 import base64
+import tempfile
 
 API_URL = "https://api-inference.huggingface.co/models/AliGhiasvand86/gisha_digit_recognition"
 headers = {"Authorization": "Bearer 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"}
 
-def query(data):
-    response = requests.post(API_URL, headers=headers, data=data)
+def query(image_path):
+    with open(image_path, "rb") as file:
+        response = requests.post(API_URL, headers=headers, files={"file": file})
     return response.json()
 
 def predict(img):
     # Convert the numpy array to a PIL Image object
     img_pil = Image.fromarray((img * 255).astype(np.uint8))
 
-    # Convert the image data to a byte array
-    img_byte_arr = io.BytesIO()
-    img_pil.save(img_byte_arr, format='PNG')
-    img_byte_arr = img_byte_arr.getvalue()
+    # Save the image to a temporary file
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_file:
+        image_path = temp_file.name
+        img_pil.save(image_path)
 
-    # Send the byte array as data in the query function
-    pred = query(img_byte_arr)
+    # Send the image file path to the query function
+    pred = query(image_path)
 
     # Convert the JSON output to a pandas DataFrame and return it
     df = pd.DataFrame(pred)
     return df
 
-iface = gr.Interface(predict, inputs = 'sketchpad',
-                     outputs = gr.outputs.Dataframe(),
-                     allow_flagging = 'never',
-                     description = 'Draw a Digit Below... (Draw in the centre for best results)')
-iface.launch(share = False, width = 300, height = 500)
+iface = gr.Interface(predict, inputs='sketchpad',
+                     outputs=gr.outputs.Dataframe(),
+                     allow_flagging='never',
+                     description='Draw a Digit Below... (Draw in the centre for best results)')
+iface.launch(share=False, width=300, height=500)
